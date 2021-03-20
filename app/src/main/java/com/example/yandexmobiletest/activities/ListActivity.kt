6deda065bf021d0444c3225fbf3bc10e1d37aca5
molodec1337322 <sshibanov1@yandex.ru>
2021-03-20 19:19:46 +0300

    some optimizations and refactor

--- app/src/main/java/com/example/yandexmobiletest/activities/ListActivity.kt
+++ app/src/main/java/com/example/yandexmobiletest/activities/ListActivity.kt
@@ -40,16 +40,67 @@ class ListActivity : AppCompatActivity() {
     private val context: Context = this
 
     private var retrofitService = Common.retrofitService
+    private var requestsCount = 0
 
     private var adapterStocks: RecyclerView.Adapter<StockAdapter.StockHolder>? = null
     private var adapterFavourites: RecyclerView.Adapter<StockAdapter.StockHolder>? = null
 
     private var stockDTOS: MutableList<StockDTO> = mutableListOf()
 
-    private var stockTickerAndDescList: MutableList<StockTickerAndDesc> = mutableListOf()
+    private var isLoading = false
 
-    private var currentShowingStocks = 0
-    private var currentShowingSearchedStocks = 0
+    private var stockTickerAndDescList: MutableList<StockTickerAndDesc> = mutableListOf(
+        StockTickerAndDesc("AAPL", "Apple Inc."), StockTickerAndDesc("BKNG", "Booking Holdings Inc."),
+        StockTickerAndDesc("MSFT", "Microsoft Corporation"), StockTickerAndDesc("AVGO", "Broadcom Limited"),
+        StockTickerAndDesc("AMZN", "Amazon.com Inc."), StockTickerAndDesc("ACN", "Accenture Plc Class A"),
+        StockTickerAndDesc("FB", "Facebook Inc. Class A"), StockTickerAndDesc("UTX", "United Technologies Corporation"),
+        StockTickerAndDesc("JPM", "JPMorgan Chase & Co."), StockTickerAndDesc("GS", "Goldman Sachs Group Inc."),
+        StockTickerAndDesc("BRK.B", "Berkshire Hathaway Inc. Class B"), StockTickerAndDesc("SLB", "Schlumberger NV"),
+        StockTickerAndDesc("JNJ", "Johnson & Johnson"), StockTickerAndDesc("CAT", "Caterpillar Inc."),
+        StockTickerAndDesc("GOOG", "Alphabet Inc. Class C"), StockTickerAndDesc("PYPL", "PayPal Holdings Inc"),
+        StockTickerAndDesc("GOOGL", "Alphabet Inc. Class A"), StockTickerAndDesc("QCOM", "QUALCOMM Incorporated"),
+        StockTickerAndDesc("XOM", "Exxon Mobil Corporation"), StockTickerAndDesc("CRM", "salesforce.com inc."),
+        StockTickerAndDesc("BAC", "Bank of America Corporation"), StockTickerAndDesc("NKE", "NIKE Inc. Class B"),
+        StockTickerAndDesc("WFC", "Wells Fargo & Company"), StockTickerAndDesc("TMO", "Thermo Fisher Scientific Inc."),
+        StockTickerAndDesc("INTC", "Intel Corporation"), StockTickerAndDesc("USB", "U.S. Bancorp"),
+        StockTickerAndDesc("T", "AT&T Inc."), StockTickerAndDesc("SBUX", "Starbucks Corporation"),
+        StockTickerAndDesc("V", "Visa Inc. Class A"), StockTickerAndDesc("LMT", "Lockheed Martin Corporation"),
+        StockTickerAndDesc("CSCO", "Cisco Systems Inc."), StockTickerAndDesc("COST", "Costco Wholesale Corporation"),
+        StockTickerAndDesc("CVX", "Chevron Corporation"), StockTickerAndDesc("MS", "Morgan Stanley"),
+        StockTickerAndDesc("UNH", "UnitedHealth Group Incorporated"), StockTickerAndDesc("PNC", "PNC Financial Services Group Inc."),
+        StockTickerAndDesc("PFE", "Pfizer Inc."), StockTickerAndDesc("LLY", "Eli Lilly and Company"),
+        StockTickerAndDesc("HD", "Home Depot Inc."), StockTickerAndDesc("UPS", "United Parcel Service Inc. Class B"),
+        StockTickerAndDesc("PG", "Procter & Gamble Company"), StockTickerAndDesc("TWX", "Time Warner Inc."),
+        StockTickerAndDesc("VZ", "Verizon Communications Inc."), StockTickerAndDesc("NEE", "NextEra Energy Inc."),
+        StockTickerAndDesc("C", "Citigroup Inc."), StockTickerAndDesc("CELG", "Celgene Corporation"),
+        StockTickerAndDesc("ABBV", "AbbVie Inc."), StockTickerAndDesc("LOW", "Lowe’s Companies Inc."),
+        StockTickerAndDesc("BA", "Boeing Company"), StockTickerAndDesc("BLK", "BlackRock Inc."),
+        StockTickerAndDesc("KO", "Coca-Cola Company"), StockTickerAndDesc("CVS", "CVS Health Corporation"),
+        StockTickerAndDesc("CMCSA", "Comcast Corporation Class A"), StockTickerAndDesc("AXP", "American Express Company"),
+        StockTickerAndDesc("MA", "Mastercard Incorporated Class A"), StockTickerAndDesc("MU", "Micron Technology Inc."),
+        StockTickerAndDesc("PM", "Philip Morris International Inc."), StockTickerAndDesc("CHTR", "Charter Communications Inc. Class A"),
+        StockTickerAndDesc("PEP", "PepsiCo Inc."), StockTickerAndDesc("SCHW", "Charles Schwab Corporation"),
+        StockTickerAndDesc("ORCL", "Oracle Corporation"), StockTickerAndDesc("MDLZ", "Mondelez International Inc. Class A"),
+        StockTickerAndDesc("DIS", "Walt Disney Company"), StockTickerAndDesc("CB", "Chubb Limited"),
+        StockTickerAndDesc("MRK", "Merck & Co. Inc."), StockTickerAndDesc("COP", "ConocoPhillips"),
+        StockTickerAndDesc("NVDA", "NVIDIA Corporation"), StockTickerAndDesc("AMAT", "Applied Materials Inc."),
+        StockTickerAndDesc("MMM", "3M Company"), StockTickerAndDesc("DHR", "Danaher Corporation"),
+        StockTickerAndDesc("AMGN", "Amgen Inc."), StockTickerAndDesc("HLT", "Hilton Worldwide Holdings Inc"),
+        StockTickerAndDesc("IBM", "International Business Machines Corporation"), StockTickerAndDesc("AMT", "American Tower Corporation"),
+        StockTickerAndDesc("NFLX", "Netflix Inc."), StockTickerAndDesc("CL", "Colgate-Palmolive Company"),
+        StockTickerAndDesc("WMT", "Walmart Inc."), StockTickerAndDesc("GD", "General Dynamics Corporation"),
+        StockTickerAndDesc("MO", "Altria Group Inc"), StockTickerAndDesc("FDX", "FedEx Corporation"),
+        StockTickerAndDesc("MCD", "McDonald’s Corporation"), StockTickerAndDesc("RTN", "Raytheon Company"),
+        StockTickerAndDesc("GE", "General Electric Company"), StockTickerAndDesc("WBA", "Walgreens Boots Alliance Inc"),
+        StockTickerAndDesc("HON", "Honeywell International Inc."), StockTickerAndDesc("NOC", "Northrop Grumman Corporation"),
+        StockTickerAndDesc("MDT", "Medtronic plc"), StockTickerAndDesc("BIIB", "Biogen Inc."),
+        StockTickerAndDesc("ABT", "Abbott Laboratories"), StockTickerAndDesc("BDX", "Becton Dickinson and Company"),
+        StockTickerAndDesc("TXN", "Texas Instruments Incorporated"), StockTickerAndDesc("ANTM", "Anthem Inc."),
+        StockTickerAndDesc("BMY", "Bristol-Myers Squibb Company"), StockTickerAndDesc("AET", "Aetna Inc."),
+        StockTickerAndDesc("ADBE", "Adobe Systems Incorporated"), StockTickerAndDesc("EOG", "EOG Resources Inc."),
+        StockTickerAndDesc("UNP", "Union Pacific Corporation"), StockTickerAndDesc("BK", "Bank of New York Mellon Corporation"),
+        StockTickerAndDesc("GILD", "Gilead Sciences Inc."), StockTickerAndDesc("ATVI", "Activision Blizzard Inc.")
+    )
     private val stocksPerPage = 10
 
     private var favourites: MutableList<StockDTO> = mutableListOf()
@@ -68,8 +119,6 @@ class ListActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
 
                 if(s.toString() != ""){
-                    currentShowingSearchedStocks = 0
-
 
                     retrofitService.getBestMatchingTickerOrName("https://finnhub.io/api/v1/search?q=${s.toString()}&token=c19j8rf48v6prmim2iog")
                         .enqueue(object : Callback<BestMatchingStockList>{
@@ -82,17 +131,6 @@ class ListActivity : AppCompatActivity() {
                                 response: Response<BestMatchingStockList>
                             ) {
                                 val responseList = response.body()
-                                //getPriceForSearchedStocks(responseList, currentShowingSearchedStocks, s.toString())
-
-                                /*
-                                val stocksTmp = stockDTOS.filter {
-                                    it.company.toLowerCase(Locale.getDefault()).startsWith(s.toString().toLowerCase(Locale.getDefault())) ||
-                                            it.ticker.toLowerCase(Locale.getDefault()).startsWith(s.toString().toLowerCase(Locale.getDefault()))
-                                }.toMutableList()
-                                val tmpAdapter = StockAdapter(stocksTmp, context)
-                                stock_recycler.swapAdapter(tmpAdapter, false)
-
-                                 */
                             }
                         })
                 }
@@ -117,13 +155,12 @@ class ListActivity : AppCompatActivity() {
             showFavourites()
         })
 
-        currentShowingStocks = 0
-        getStocksList()
         stock_recycler = recycle_list_stock
         stock_recycler.setHasFixedSize(true)
         val layoutManager = LinearLayoutManager(context)
         stock_recycler.layoutManager = layoutManager
         adapterStocks = StockAdapter(stockDTOS, context)
+        getStocksList()
         stock_recycler.adapter = adapterStocks
 
         stock_recycler.addOnScrollListener(object: RecyclerView.OnScrollListener(){
@@ -133,12 +170,9 @@ class ListActivity : AppCompatActivity() {
                     //hideProgressBar()
                 }
                 if(dy > 0){
-                    if(layoutManager.childCount + layoutManager.findFirstVisibleItemPosition() >= layoutManager.itemCount
-                        && currentShowingStocks <= layoutManager.itemCount){
-                        //getPriceForStocks(stocksListResponse, currentShowingStocks)
-                        getPriceForStocks(stockTickerAndDescList, adapterStocks!!, currentShowingStocks)
+                    if(layoutManager.childCount + layoutManager.findFirstVisibleItemPosition() >= layoutManager.itemCount && !isLoading){
+                        getPriceForStocks(stockTickerAndDescList, adapterStocks!!)
                         showProgressBar()
-                        currentShowingStocks += stocksPerPage
                     }
                 }
             }
@@ -176,10 +210,12 @@ class ListActivity : AppCompatActivity() {
     }
 
     fun showProgressBar(){
+        isLoading = true
         progress_Bar.visibility = View.VISIBLE
     }
 
     fun hideProgressBar(){
+        isLoading = false
         progress_Bar.visibility = View.INVISIBLE
     }
 
@@ -189,6 +225,8 @@ class ListActivity : AppCompatActivity() {
 
     fun getStocksList(){
         showProgressBar()
+        getPriceForStocks(stockTickerAndDescList, adapterStocks!!)
+        /*
         retrofitService.getStocksResponseList().enqueue(object : Callback<StocksList>{
             override fun onFailure(call: Call<StocksList>, t: Throwable) {
                 Toast.makeText(context, "Failed connect to server", Toast.LENGTH_SHORT).show()
@@ -209,16 +247,16 @@ class ListActivity : AppCompatActivity() {
                 currentShowingStocks += stocksPerPage
             }
         })
+         */
     }
 
     fun getPriceForStocks(
         tickerAndDescList: MutableList<StockTickerAndDesc>,
-        adapter: RecyclerView.Adapter<StockAdapter.StockHolder>,
-        currentShowingStocks: Int
+        adapter: RecyclerView.Adapter<StockAdapter.StockHolder>
     ){
-        var requestsCount = 0
         var currentTicker: StockTickerAndDesc
-        for(i in currentShowingStocks until currentShowingStocks + stocksPerPage){
+        val currentLoadedStocks = stockDTOS.size
+        for(i in currentLoadedStocks until currentLoadedStocks + stocksPerPage){
             if(i < tickerAndDescList.size){
                 currentTicker = tickerAndDescList[i]
             }
@@ -228,44 +266,56 @@ class ListActivity : AppCompatActivity() {
 
             showProgressBar()
             requestsCount++
-            retrofitService.getOpenCloseStockPrice("https://finnhub.io/api/v1/quote?symbol=${currentTicker.ticker}&token=c19j8rf48v6prmim2iog")
-                .enqueue(object : Callback<StockPrice>{
-                    override fun onFailure(call: Call<StockPrice>, t: Throwable) {
+            getPrice(currentTicker, adapter)
+        }
+    }
 
-                    }
+    fun getPrice(stock: StockTickerAndDesc, adapter: RecyclerView.Adapter<StockAdapter.StockHolder>){
+        retrofitService.getOpenCloseStockPrice("https://finnhub.io/api/v1/quote?symbol=${stock.ticker}&token=c19j8rf48v6prmim2iog")
+            .enqueue(object : Callback<StockPrice>{
+                override fun onFailure(call: Call<StockPrice>, t: Throwable) {
+                    Toast.makeText(context, "Failed connect to server", Toast.LENGTH_SHORT).show()
+                }
 
-                    override fun onResponse(
-                        call: Call<StockPrice>,
-                        response: Response<StockPrice>
-                    ) {
-                        if(response.body() != null){
-                            val priceChange: String
-                            if(String.format("%.2f", response.body()!!.o - response.body()!!.c).toDouble() > 0){
-                                priceChange = String.format("+%.2f", response.body()!!.o - response.body()!!.c)
-                            }
-                            else if(String.format("%.2f", response.body()!!.o - response.body()!!.c).toDouble() < 0){
-                                priceChange = String.format("%.2f", response.body()!!.o - response.body()!!.c)
-                            }
-                            else{
-                                priceChange = String.format("%.2f", response.body()!!.o - response.body()!!.c)
-                            }
-
-                            stockDTOS.add(
-                                StockDTO(
-                                    currentTicker.ticker,
-                                    currentTicker.description,
-                                    response.body()!!.c.toString(),
-                                    priceChange
-                                )
-                            )
-                            adapter.notifyDataSetChanged()
-                            requestsCount--
-                            if(requestsCount == 0){
-                                hideProgressBar()
-                            }
+                override fun onResponse(
+                    call: Call<StockPrice>,
+                    response: Response<StockPrice>
+                ) {
+                    if(response.errorBody() != null){
+                        requestsCount--
+                        if(requestsCount == 0){
+                            hideProgressBar()
+                            Toast.makeText(context, "Failed connect to server", Toast.LENGTH_SHORT).show()
                         }
                     }
-                })
-        }
+
+                    if(response.body() != null){
+                        val priceChange: String
+                        if(String.format("%.2f", response.body()!!.o - response.body()!!.c).toDouble() > 0){
+                            priceChange = String.format("+%.2f", response.body()!!.o - response.body()!!.c)
+                        }
+                        else if(String.format("%.2f", response.body()!!.o - response.body()!!.c).toDouble() < 0){
+                            priceChange = String.format("%.2f", response.body()!!.o - response.body()!!.c)
+                        }
+                        else{
+                            priceChange = String.format("%.2f", response.body()!!.o - response.body()!!.c)
+                        }
+
+                        stockDTOS.add(
+                            StockDTO(
+                                stock.ticker,
+                                stock.description,
+                                response.body()!!.c.toString(),
+                                priceChange
+                            )
+                        )
+                        adapter.notifyDataSetChanged()
+                        requestsCount--
+                        if(requestsCount == 0){
+                            hideProgressBar()
+                        }
+                    }
+                }
+            })
     }
 }
